374b4afa21f9be0b9b7e23ad3af97208b0cce61b
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2022-12-19 18:22:24 +0100

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,17 +17,17 @@
 // };
 
 export const environment = {
-    production: false,
-    firebase: {
-      apiKey: "AIzaSyBwHS57ahusU-MJriq5mlzSAICeDc4VxWA",
-      authDomain: "music4all-8cfdb.firebaseapp.com",
-      projectId: "music4all-8cfdb",
-      storageBucket: "music4all-8cfdb.appspot.com",
-      messagingSenderId: "747122516654",
-      appId: "1:747122516654:web:be98f9f26e813515cac33f",
-      measurementId: "G-9RQRLLMBMB"
-    }
-  };
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBwHS57ahusU-MJriq5mlzSAICeDc4VxWA",
+    authDomain: "music4all-8cfdb.firebaseapp.com",
+    projectId: "music4all-8cfdb",
+    storageBucket: "music4all-8cfdb.appspot.com",
+    messagingSenderId: "747122516654",
+    appId: "1:747122516654:web:be98f9f26e813515cac33f",
+    measurementId: "G-9RQRLLMBMB"
+  }
+};
 
   /*
    * For easier debugging in development mode, you can import the following file
